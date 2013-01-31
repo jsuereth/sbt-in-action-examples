157a0d6d43466b8d60d62a860e1130d4aeb87c9f
Josh Suereth <joshua.suereth@gmail.com>
2013-01-30 23:04:51 -0500

    Adding test for example #2

--- chapter1/src/test/scala/LogicSpec.scala
+++ chapter1/src/test/scala/LogicSpec.scala
@@ -7,5 +7,11 @@ object LogicSpec extends Specification {
       val prefs = BuyerPreferences(List("male", "tabby"))
       Logic.matchLiklihood(tabby, prefs) must beGreaterThan(.999)
     }
+    "be 0% when no attributes match" in {
+      val tabby = Kitten("1", List("male", "tabby"))
+      val prefs = BuyerPreferences(List("female", "calico"))
+      val result = Logic.matchLiklihood(tabby, prefs) 
+      result must beLessThan(.001)
+    }
   }
 }
