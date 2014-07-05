e1cd8c1c69ec02416567286c46e62939869922ce
Josh Suereth <joshua.suereth@gmail.com>
2014-07-05 10:04:20 -0400

    Add tests from chapter into build.

---
+++ chapter3/analytics/src/main/scala/logic.scala
@@ -0,0 +1,10 @@
+object Logic {
+  /** Determines the match likelihood and returns % match. */
+  def matchLikelihood(kitten: Kitten, buyer: BuyerPreferences): Double = {
+    val matches = buyer.attributes map { attribute => 
+      kitten.attributes contains attribute
+    }
+    val nums = matches map { b => if(b) 1.0 else 0.0 }
+    nums.sum / nums.length
+  }
+}
