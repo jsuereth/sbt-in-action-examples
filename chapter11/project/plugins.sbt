19a4081c4ff806876dcda7094eb9ce5cf39581c5
Matthew Farwell <matthew@farwell.co.uk>
2014-07-07 15:41:51 +0200

    Redone HelloPlugin

--- chapter11/project/plugins.sbt
+++ chapter11/project/plugins.sbt
@@ -1,4 +1,4 @@
-resolvers += "typesafe-release" at "http://typesafe.artifactoryonline.com/typesafe/repo/"
+// resolvers += "typesafe-release" at "http://typesafe.artifactoryonline.com/typesafe/repo/"
 
 libraryDependencies <+= (sbtVersion) { sv =>
   "org.scala-sbt" % "scripted-plugin" % sv
--- chapter11/src/main/scala/HelloPlugin.scala
+++ chapter11/src/main/scala/HelloPlugin.scala
@@ -1,53 +1,21 @@
 
 package org.preownedkittens.sbt
 
-
-import java.util.Date
-import java.util.jar.JarEntry
-import java.util.jar.JarFile
-import scala.io.Codec
-import sbt.Compile
-import sbt.ConfigKey.configurationToKey
-import sbt.File
-import sbt.IO
-import sbt.InputKey
-import sbt.Keys.scalaSource
-import sbt.Keys.streams
-import sbt.Keys.target
-import sbt.Logger
-import sbt.Plugin
-import sbt.Project
-import sbt.Scoped.t3ToTable3
-import sbt.Scoped.t6ToTable6
-import sbt.SettingKey
-import sbt.TaskKey
-import sbt.file
-import sbt.inputTask
-import sbt.richFile
-import sbt.std.TaskStreams
-import sbt.ScopedKey
-import sbt.AutoPlugin
-import sbt.PluginTrigger
-import sbt.Plugins
+import sbt._
+import sbt.Keys._
 
 object HelloPlugin extends sbt.AutoPlugin {
-  // override def trigger: PluginTrigger = Plugins.allRequirements
-
-  // import autoImport._
+  lazy val helloTask = InputKey[Unit]("hello", "Prints Hello world.")
+  lazy val helloKey = SettingKey[String]("default message for hello")
  
   override def projectSettings = Seq(
     helloTask <<= inputTask {
       (argTask: TaskKey[Seq[String]]) => {
-        (argTask, scalaSource, helloKey, streams) map {
-          (args, sourceDir, helloKeyValue, streams) => { streams.log.info("Hello " + sourceDir.getAbsolutePath) }
+        (argTask, scalaSource in Compile, helloKey, streams) map {
+          (args, sourceDir, helloKeyValue, streams) => { streams.log.info("Hello " + helloKeyValue + " " + args.mkString(",") + " " + sourceDir.getAbsolutePath) }
         }
       }
     },
-    helloKey := "default value"
+    helloKey := "default message"
   )
-
-  //object autoImport {
-		lazy val helloTask = InputKey[Unit]("hello", "Prints Hello world.")
-    lazy val helloKey = SettingKey[String]("default value for hello world")
-  //} 
 }
